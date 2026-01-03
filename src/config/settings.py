794447b9787eb089c0c7847f645a7356e273de0f
ppincet <pavel.v.vashkevich@gmail.com>
2026-01-03 16:03:37 +0100

    upd

---
+++ src/config/settings.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+
+try:
+    from dotenv import load_dotenv
+    env_path = Path(__file__).resolve().parent.parent / '.env'
+    if env_path.exists():
+        print(f"Loading environment from {env_path}")
+        load_dotenv(dotenv_path=env_path, override=True)
+except ImportError:
+    pass
+
+class Settings:
+    try:
+        CONSUMER_KEY = os.environ['SF_CONSUMER_KEY']
+        USERNAME     = os.environ['SF_USERNAME']
+        LOGIN_URL    = os.environ['SF_LOGIN_URL']
+        _raw_key = os.environ['SF_PRIVATE_KEY']
+        PRIVATE_KEY = _raw_key.replace('\\n', '\n') if '\\n' in _raw_key else _raw_key
+        SSH_USERNAME = os.environ['SSH_USERNAME']
+        SSH_PASSWORD = os.environ['SSH_PASSWORD']
+        SSH_HOST     = os.environ['SSH_HOST']
+
+    except KeyError as e:
+        raise RuntimeError(f"❌ CRITICAL ERROR: Missing environment variable {e}") from e
+    WINDOW             = float(os.getenv('WINDOW', '3600')) # Default to 1 hour
+    DEBUG              = os.getenv('DEBUG', 'False').lower() == 'true'
+    SSH_REMOTE_IFOLDER = os.getenv('SSH_REMOTE_IFOLDER', '/inbound')
+    SSH_REMOTE_UFOLDER = os.getenv('SSH_REMOTE_UFOLDER', '/outbound')
+    SSH_FILE_IPREFIX   = os.getenv('SSH_FILE_IPREFIX', 'data_')
+
+settings = Settings()
+
+
+
+# import os
+# from pathlib import Path
+# from dotenv import load_dotenv
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# env_path = BASE_DIR / '.env'
+# load_dotenv(dotenv_path=env_path)
+# CONSUMER_KEY = os.getenv('SF_CONSUMER_KEY')
+# USERNAME = os.getenv('SF_USERNAME')
+# PRIVATE_KEY = os.getenv('SF_PRIVATE_KEY').replace('\\n', '\n') 
+# LOGIN_URL = os.getenv('SF_LOGIN_URL')
+# WINDOW = os.getenv('WINDOW')
+# DEBUG = os.getenv('DEBUG')
+# SSH_USERNAME = os.getenv('SSH_USERNAME')
+# SSH_PASSWORD = os.getenv('SSH_PASSWORD')
+# SSH_HOST = os.getenv('SSH_HOST')
+# SSH_REMOTE_IFOLDER = os.getenv('SSH_REMOTE_IFOLDER')
+# SSH_REMOTE_UFOLDER = os.getenv('SSH_REMOTE_UFOLDER')
+# SSH_FILE_IPREFIX = os.getenv('SSH_FILE_IPREFIX')
