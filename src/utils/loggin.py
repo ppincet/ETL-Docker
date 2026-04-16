c4169dc5ddcbe92ea4b1f7899ec9fd2a55178980
ppincet <pavel.v.vashkevich@gmail.com>
2026-04-16 13:21:23 +0200

    main

--- src/utils/loggin.py
+++ src/utils/loggin.py
@@ -4,31 +4,36 @@ from config import settings
 import os
 debug = settings.DEBUG
 def setup_logging(log_file="etl.log", level=logging.INFO):
-    log_dir = 'app/logs'
-    os.makedirs(log_dir, exist_ok=True)
-    log_path = os.path.join(log_dir, log_file)
-    file_handler = RotatingFileHandler(
-        log_path, 
-        maxBytes = settings.SYS_MAX_LOG_SIZE, 
-        backupCount = settings.SYS_MAX_LOG_ROTATION
-    )
-    file_handler.setLevel(level)
+    try:
 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.WARNING) 
+        log_dir = 'app/logs'
+        os.makedirs(log_dir, exist_ok=True)
+        log_path = os.path.join(log_dir, log_file)
+        file_handler = RotatingFileHandler(
+            log_path, 
+            maxBytes = settings.SYS_MAX_LOG_SIZE, 
+            backupCount = settings.SYS_MAX_LOG_ROTATION
+        )
+        file_handler.setLevel(level)
 
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-    file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
-    logging.basicConfig(
-        level=level,
-        handlers=[file_handler, console_handler],
-        force=True
-    )
-    # min level is warning & up to critical
-    logging.getLogger("paramiko").setLevel(logging.WARNING)
-    if debug:
-        logging.info("Logging initialized successfully.")
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.WARNING) 
+
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+        logging.basicConfig(
+            level=level,
+            handlers=[file_handler, console_handler],
+            force=True
+        )
+        # min level is warning & up to critical
+        logging.getLogger("paramiko").setLevel(logging.WARNING)
+        if debug:
+            logging.info("Logging initialized successfully.")
+    except Exception as e:
+        print(f'exc from logging: {e}')
+        raise
